869135a6c29ff7c0ec8895b23faba693ac817b6b
tania666 <37729922+tania666@users.noreply.github.com>
2021-02-18 13:26:34 +0700

    Update settings.py

--- djmysql/settings.py
+++ djmysql/settings.py
@@ -76,22 +76,14 @@ WSGI_APPLICATION = 'djmysql.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
 }
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DABATASES['default'].update(db_from_env)
+
+DATABASES['default'] = dj_database_url.config(default='postgres://...'}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
